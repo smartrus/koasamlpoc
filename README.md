ed45a45bf6bfe5ccd1b62810c2e9dd596f778277
Rustem Abdrakhmanov <rustemabd@Rustems-MacBook-Pro.local>
2019-07-27 16:16:14 +0400

    Initial upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# koasamlpoc
+# koa-saml-poc
+
+Run the commands below:
+
+```
+npm install
+npm start
+```
+
+Navigate to `localhost:8080` for SAML authentication. 
---
+++ passport.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Strategy = require('passport-saml').Strategy;
+const passport = require('koa-passport');
+const fs = require('fs');
+
+// Update the variables below with correct values:
+const entryPoint = 'my-entryPoint-url';
+const issuer = 'my_issuer_name';
+const pathToPublicCert = '/path/to/my.cert';
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+function onProfile(profile, cb) {
+  return cb(null, { email: profile.nameID });
+}
+
+const samlConf = {
+  path: '/auth/saml/callback',
+  entryPoint: 'https://login.microsoftonline.com/e5dbf331-8e01-4865-af58-3b73eeb5b8fe/saml2',
+  issuer: 'smala',
+  cert: fs.readFileSync('Smala.cer').toString(),
+};
+
+passport.use(new Strategy(samlConf, onProfile));
+
+module.exports = passport;
